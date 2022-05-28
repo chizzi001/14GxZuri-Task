83861e23f8919cc3409c73dce0d795db55084f87
chizzi001 <chidiobinna0001@gmail.com>
2022-05-28 01:58:29 +0100

    Completed Read Text File Assignment

--- Python/Reading-Text-Files/main.py
+++ Python/Reading-Text-Files/main.py
@@ -3,14 +3,23 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def read_file_content(filename):
+def read_file_content(story):
     # [assignment] Add your code here 
-    
-    return "Hello World"
+    with open(story) as f:
+        text = f.read()
+        
+    return text
 
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
+    splited_text = text.split()
+    totalWordCount ={}
+    for word in splited_text:
+        totalWordCount[word] = splited_text.count(word)
+    return totalWordCount
 
-    return {"as": 10, "would": 20}
+print(count_words())
+
+    # return {"as": 10, "would": 20}
