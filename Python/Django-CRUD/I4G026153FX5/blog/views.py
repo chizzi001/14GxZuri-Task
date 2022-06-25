482e0894e5a8bd20221c425d5ef63b8b409930d3
chizzi001 <chidiobinna0001@gmail.com>
2022-06-25 23:10:59 +0100

    Updated Django

--- Python/Django-CRUD/I4G026153FX5/blog/views.py
+++ Python/Django-CRUD/I4G026153FX5/blog/views.py
@@ -8,29 +8,33 @@ from django.urls import reverse_lazy
 from blog.models import Post
 
 
-
-
 # Create your views here.
 
 class PostListView(generic.ListView):
     model = Post
-    template_name = ".html"
+    template_name = "blog/post_list.html"
 
 class PostCreateView(generic.CreateView):
     model = Post
-    template_name = ".html"
+    template_name = "blog/post_form.html"
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
 
 class PostDetailView(generic.DetailView):
     model = Post
-    template_name = ".html"
+    template_name = "blog/post_detail.html"
 
 
 class PostUpdateView(generic.UpdateView):
     model = Post
-    template_name = ".html"
+    template_name = "blog/post_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
